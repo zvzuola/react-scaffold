80208532da3954c6419eb6e3ba09820d5d7f0289
zvzuola <15257109820@163.com>
2018-05-08 17:16:15 +0800

    feat: 开发环境抽取vendor,manifest

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,21 @@ let config = {
     }),
     new CopyWebpackPlugin([
       { from: path.resolve(__dirname, './static_file') } // 把static_file的文件拷到对应的output目录下
-    ])
+    ]),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+        if (module.resource && (/^.*\.(css|scss|less|jpe?g|png|gif|eot|woff|woff2|svg|ttf)$/).test(module.resource)) {
+          return false;
+        }
+        // this assumes your vendor imports exist in the node_modules directory
+        return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    }),
+    //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+    })
   ],
   stats: {
     colors: true // Nice colored output
@@ -76,20 +90,6 @@ if (process.env.NODE_ENV === 'production') {
     new CleanWebpackPlugin(['./public/*.*']),
     new webpack.optimize.UglifyJsPlugin(),
     new ExtractTextPlugin("index-[hash].css"),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module) {
-        if (module.resource && (/^.*\.(css|scss|less|jpe?g|png|gif|eot|woff|woff2|svg|ttf)$/).test(module.resource)) {
-          return false;
-        }
-        // this assumes your vendor imports exist in the node_modules directory
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      }
-    }),
-    //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production')
